2ae989029a99bb3485a35cfb02d3e53d267b0b6e
Cali Weber <calista.weber1@gmail.com>
2023-08-02 15:21:58 -0700

    finished exercise

--- StaticExercise/Program.cs
+++ StaticExercise/Program.cs
@@ -4,7 +4,11 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var tempftc = TempConverter.FahrenheittoCelcius(98);
+            var tempctf = TempConverter.CelciustoFahrenheit(36);
+
+            Console.WriteLine($"This it the temp today in Fahrenheit {tempctf}");
+            Console.WriteLine($"This is the temp today in Celcius {tempftc}");
         }
     }
 }
---
+++ StaticExercise/TempConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticExercise
+{
+    public static class TempConverter
+    {
+        public static double FahrenheittoCelcius(double fahrenheit)
+        {
+            var tempftc = (fahrenheit - 32) * 5/9;
+            return tempftc;
+        }
+
+        public static double CelciustoFahrenheit(double celcius) 
+        {
+            var tempctf = (celcius * 9/5) + 32;
+            return tempctf;
+        }   
+    }
+
+
+}
